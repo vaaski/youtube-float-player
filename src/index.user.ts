504363960c884ec38744b440b3e7bab516bc11fb
vaaski <admin@vaa.ski>
2024-02-16 20:15:24 +0100

    floating player positioning

--- src/index.user.ts
+++ src/index.user.ts
@@ -4,7 +4,13 @@ import { DynamicCSS } from "./dynamic-css"
 import { awaitElement } from "./spawncamp"
 
 const playerCSS = new DynamicCSS("ytd-player#ytd-player.floating")
-playerCSS.update({ position: "fixed", "z-index": "999" })
+playerCSS.update({
+  position: "fixed",
+  "z-index": "1001",
+  "border-radius": "12px",
+  overflow: "hidden",
+  "box-shadow": "1px 1px 25px rgba(0,0,0,.5)",
+})
 
 const sizeCSS = new DynamicCSS(
   ["ytd-player#ytd-player.floating", "ytd-player#ytd-player video.floating"].join(",")
@@ -17,6 +23,7 @@ const hideCSS = new DynamicCSS(
   [
     "ytd-player#ytd-player .ytp-chrome-bottom.floating",
     "ytd-player#ytd-player .iv-branding",
+    "ytd-player#ytd-player .ytp-gradient-bottom",
   ].join(",")
 )
 hideCSS.update({ display: "none" })
@@ -39,7 +46,9 @@ const main = async () => {
 
     const width = sidebar.clientWidth
     const height = (sidebar.clientWidth / clientWidth) * clientHeight
+    const left = sidebar.getBoundingClientRect().left
     sizeCSS.update({ width: `${width}px`, height: `${height}px` })
+    playerCSS.update({ left: `${left}px`, top: "80px" })
 
     ytdPlayer.classList.toggle("floating", state)
     // eslint-disable-next-line unicorn/consistent-destructuring
@@ -61,5 +70,4 @@ const main = async () => {
   console.log("observing", playerParent)
 }
 
-// eslint-disable-next-line unicorn/prefer-top-level-await
 main().catch(alert)
